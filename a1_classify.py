4792e4fb297fa8a2947150d2f5bc90107e9c4fb0
Modiri Arghavan <modiriar@teach.cs.toronto.edu>
2020-02-07 09:55:35 -0500

    classes 31, 32, and 33 are complete.

--- a1_classify.py
+++ a1_classify.py
@@ -12,6 +12,17 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import SGDClassifier  
 
+clf_names = ['SGDClassifier', 
+                'GaussianNB', 
+                'RandomForestClassifier',
+                'MLPClassifier',
+                'AdaBoostClassifier']
+clfs = [SGDClassifier(loss='hinge'),
+        GaussianNB(),
+        RandomForestClassifier(max_depth=5, n_estimators=10),
+        MLPClassifier(alpha=0.05),
+        AdaBoostClassifier()]
+
 
 def accuracy(C):
     ''' Compute accuracy given Numpy array confusion matrix C. Returns a floating point value '''
@@ -24,9 +35,11 @@ def recall(C):
     ''' Compute recall given Numpy array confusion matrix C. Returns a list of floating point values '''
     r = []
     for label in range(len(C)):
-      row = C[label, :]
-      r.append(C[label, label] / row.sum())
-
+        row = C[label, :].sum()
+        if row != 0:
+            r.append(C[label, label] / row)
+        else:
+            r.append(0.0)
     return r
 
 
@@ -34,8 +47,11 @@ def precision(C):
     ''' Compute precision given Numpy array confusion matrix C. Returns a list of floating point values '''
     p = []
     for label in range(len(C)):
-      col = C[:, label]
-      p.append(C[label, label] / col.sum())
+      col = C[:, label].sum()
+      if col != 0:
+        p.append(C[label, label] / col)
+      else:
+        p.append(0.0)
 
     return p
 
@@ -53,21 +69,36 @@ def class31(output_dir, X_train, X_test, y_train, y_test):
        i: int, the index of the supposed best classifier
     '''
     print('TODO Section 3.1')
-    print(X_train)
-    clf = []
-    clf[0] = SGDClassifier(loss='hinge')
-    clf[0] .fit(X_train, y_train)
-    y_pred = clf[0].predict(X_test)
-    c_matrix = confusion_matrix(y_test, y_pred)
+    c_matrices = []
+    acc_list = []
+    for clf in clfs:
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        c_temp = confusion_matrix(y_test, y_pred)
+        c_matrices.append(c_temp)
+        acc_list.append(accuracy(c_temp))
 
+    best_acc = acc_list[0]
     iBest = 0
+    for i in range(len(acc_list)):
+        if acc_list[i] > best_acc:
+            best_acc = acc_list[i]
+            iBest = i
+
     with open(f"{output_dir}/a1_3.1.txt".format(output_dir), "w") as outf:
         # For each classifier, compute results and write the following output:
-        #     outf.write(f'Results for {classifier_name}:\n')  # Classifier name
-        #     outf.write(f'\tAccuracy: {accuracy:.4f}\n')
-        #     outf.write(f'\tRecall: {[round(item, 4) for item in recall]}\n')
-        #     outf.write(f'\tPrecision: {[round(item, 4) for item in precision]}\n')
-        #     outf.write(f'\tConfusion Matrix: \n{conf_matrix}\n\n')
+        for clf_name, clf, c_matrix in zip(clf_names, clfs, c_matrices):
+            # outf.write(f'Results for {classifier_name}:\n')  # Classifier name
+            outf.write(f'Results for {clf_name}:\n')  # Classifier name
+            # outf.write(f'\tAccuracy: {accuracy:.4f}\n')
+            outf.write(f'\tAccuracy: {accuracy(c_matrix):.4f}\n')
+            # outf.write(f'\tRecall: {[round(item, 4) for item in recall]}\n')
+            outf.write(f'\tRecall: {[round(item, 4) for item in recall(c_matrix)]}\n')
+            # outf.write(f'\tPrecision: {[round(item, 4) for item in precision]}\n')
+            outf.write(f'\tPrecision: {[round(item, 4) for item in precision(c_matrix)]}\n')
+            # outf.write(f'\tConfusion Matrix: \n{conf_matrix}\n\n')
+            outf.write(f'\tConfusion Matrix: \n{c_matrix}\n\n')
+
         pass
 
     return iBest
@@ -89,11 +120,28 @@ def class32(output_dir, X_train, X_test, y_train, y_test, iBest):
        y_1k: numPy array, just 1K rows of y_train
    '''
     print('TODO Section 3.2')
-    
+    sizes = [1000, 5000, 10000, 15000, 20000]
+    c_matrices = []
+    acc_list = []
+    X_tr, x, y_tr, x = train_test_split(X_train, y_train, train_size=20000)
+    X_1k = X_tr[:1000]
+    y_1k = y_tr[:1000]
+    for s in sizes:
+        #X_train_new = np.random.choice(X_train, size=s, replace=False)
+        X_tr_temp =X_tr[:s]
+        y_tr_temp = y_tr[:s]
+        clfs[iBest].fit(X_tr_temp, y_tr_temp)
+        y_pr = clfs[iBest].predict(X_test)
+        c_temp = confusion_matrix(y_test, y_pr)
+        c_matrices.append(c_temp)
+        acc_list.append(accuracy(c_temp))
+
     with open(f"{output_dir}/a1_3.2.txt", "w") as outf:
         # For each number of training examples, compute results and write
-        # the following output:
-        #     outf.write(f'{num_train}: {accuracy:.4f}\n'))
+        for num_train, acc in zip(sizes, acc_list):
+            # the following output:
+            # outf.write(f'{num_train}: {accuracy:.4f}\n'))
+            outf.write(f'{num_train}: {acc:.4f}\n')
         pass
 
     return (X_1k, y_1k)
@@ -113,6 +161,8 @@ def class33(output_dir, X_train, X_test, y_train, y_test, i, X_1k, y_1k):
        y_1k: numPy array, just 1K rows of y_train (from task 3.2)
     '''
     print('TODO Section 3.3')
+    #p_values = []
+
     
     with open(f"{output_dir}/a1_3.3.txt", "w") as outf:
         # Prepare the variables with corresponding names, then uncomment
@@ -120,12 +170,41 @@ def class33(output_dir, X_train, X_test, y_train, y_test, i, X_1k, y_1k):
         
         # for each number of features k_feat, write the p-values for
         # that number of features:
-            # outf.write(f'{k_feat} p-values: {[round(pval, 4) for pval in p_values]}\n')
-        
+        for k_feat in {5,50}:
+            selector = SelectKBest(f_classif, k_feat)
+            X_new = selector.fit_transform(X_train, y_train)
+            p_values = selector.pvalues_
+            outf.write(f'{k_feat} p-values: {[round(pval, 4) for pval in p_values]}\n')
+
+        selector = SelectKBest(f_classif, 5)
         # outf.write(f'Accuracy for 1k: {accuracy_1k:.4f}\n')
+        X_new = selector.fit_transform(X_1k, y_1k)
+        selected_cols_1k = selector.get_support(indices=True)
+        print("selected_cols_1k : ", selected_cols_1k)
+        X_test_new = selector.transform(X_test)
+
+        clfs[i].fit(X_new, y_1k)
+        y_pr = clfs[i].predict(X_test_new)
+        c = confusion_matrix(y_test, y_pr)
+        accuracy_1k = accuracy(c)
+        outf.write(f'Accuracy for 1k: {accuracy_1k:.4f}\n')
+        selected_cols_1k = selector.get_support(indices=True)
+        print("selected_cols_1k : ", selected_cols_1k)
+
         # outf.write(f'Accuracy for full dataset: {accuracy_full:.4f}\n')
-        # outf.write(f'Chosen feature intersection: {feature_intersection}\n')
-        # outf.write(f'Top-5 at higher: {top_5}\n')
+        X_new = selector.fit_transform(X_train, y_train)
+        X_test_new = selector.transform(X_test)
+        clfs[i].fit(X_new, y_train)
+        y_pr = clfs[i].predict(X_test_new)
+        c = confusion_matrix(y_test, y_pr)
+        accuracy_full = accuracy(c)
+        outf.write(f'Accuracy for full dataset: {accuracy_full:.4f}\n')
+        selected_cols_full = selector.get_support(indices=True)
+        print("selected_cols_full : ", selected_cols_full)
+
+        feature_intersection = list(set(selected_cols_1k).intersection(selected_cols_full))
+        outf.write(f'Chosen feature intersection: {feature_intersection}\n')
+        outf.write(f'Top-5 at higher: {selected_cols_full}\n')
         pass
 
 
@@ -150,7 +229,6 @@ def class34(output_dir, X_train, X_test, y_train, y_test, i):
         pass
 
 
-    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="the input npz file from Task 2", required=True)
@@ -187,5 +265,10 @@ if __name__ == "__main__":
     print(one)
     print(two)
     print(three)
-    class31(output_dir, X_train, X_test, y_train, y_test)
+    print("BESSSSSTTTT")
+    #best_clf = class31(output_dir, X_train, X_test, y_train, y_test)
+    best_clf = 4
+
+    (X_1k, y_1k) = class32(output_dir, X_train, X_test, y_train, y_test, best_clf)
     # TODO : complete each classification experiment, in sequence.
+    class33(output_dir, X_train, X_test, y_train, y_test, best_clf, X_1k, y_1k)
